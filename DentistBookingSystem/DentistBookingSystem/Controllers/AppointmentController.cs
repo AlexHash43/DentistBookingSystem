84fa53e0da315fe0d64144c58ce505453fb6c611
Mateusz Siluk <65629709+MateuszSiluk@users.noreply.github.com>
2021-05-15 15:53:05 +0100

    ShowDataInControlerTest

---
+++ DentistBookingSystem/DentistBookingSystem/Controllers/AppointmentController.cs
@@ -0,0 +1,30 @@
+﻿using DentistBookingSystem.DataAccess;
+using DentistBookingSystem.DataAccess.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DentistBookingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IRepository<Appointment> appointmentRepository;
+        public AppointmentController(IRepository<Appointment> appointmentRepository)
+        {
+            this.appointmentRepository = appointmentRepository;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Appointment> GetAllApointments() => this.appointmentRepository.GetAll();
+
+        [HttpGet]
+        [Route("{appointmentId}")]
+        public Appointment GetAppointmentById(int appointmentId) => this.appointmentRepository.GetById(appointmentId);
+    }
+}
