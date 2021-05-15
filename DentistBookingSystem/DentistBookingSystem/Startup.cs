b17b356245f531e9fc2813979c1531eabc4b7bef
Mateusz Siluk <65629709+MateuszSiluk@users.noreply.github.com>
2021-05-15 15:15:46 +0100

    AddedScopedToConfiguration

--- DentistBookingSystem/DentistBookingSystem/Startup.cs
+++ DentistBookingSystem/DentistBookingSystem/Startup.cs
@@ -1,3 +1,4 @@
+using DentistBookingSystem.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace DentistBookingSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
