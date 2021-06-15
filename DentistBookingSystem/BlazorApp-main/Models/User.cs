f90707bae69df0cd250445a57eb30a6f43669bb3
Mateusz Siluk <65629709+MateuszSiluk@users.noreply.github.com>
2021-06-15 18:31:05 +0100

    BlazzorUpdateTable

--- DentistBookingSystem/BlazorApp-main/Models/User.cs
+++ DentistBookingSystem/BlazorApp-main/Models/User.cs
@@ -4,8 +4,9 @@ namespace BlazorApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Surename { get; set; }
+        public string Surname { get; set; }
         public string UserName { get; set; }
+        public string Role { get; set; }
         public string AuthData { get; set; }
     }
 }
