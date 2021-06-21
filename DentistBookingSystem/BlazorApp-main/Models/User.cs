1c728b58bdffdc265ba0a66e506cc6820aef4882
Mateusz Siluk <65629709+MateuszSiluk@users.noreply.github.com>
2021-06-21 19:00:05 +0100

    addedValidationToRegistration

--- DentistBookingSystem/BlazorApp-main/Models/User.cs
+++ DentistBookingSystem/BlazorApp-main/Models/User.cs
@@ -6,16 +6,24 @@ namespace BlazorApp.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
         public string Name { get; set; }
+        [Required]
         public string Surname { get; set; }
         public string UserName { get; set; }
         public string Role { get; set; } = "Patient";
         public string AuthData { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public DateTime BirthDate { get; set; } = DateTime.Today;
+        [Required]
         public string PhoneNumber { get; set; }
         public DateTime DateRegistered { get; set; } = DateTime.Now;
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string Gender { get; set; }
 
         
