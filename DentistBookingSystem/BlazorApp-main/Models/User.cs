31df479108dcd246ca6fccc81b58565312fb993e
Mateusz Siluk <65629709+MateuszSiluk@users.noreply.github.com>
2021-06-26 14:37:46 +0100

    Update Annotations User

--- DentistBookingSystem/BlazorApp-main/Models/User.cs
+++ DentistBookingSystem/BlazorApp-main/Models/User.cs
@@ -22,7 +22,10 @@ namespace BlazorApp.Models
         public string PhoneNumber { get; set; }
         public DateTime DateRegistered { get; set; } = DateTime.Now;
         [Required]
-        [Range(8,50)]
+        [StringLength(26, MinimumLength = 8)]
+        /*[RegularExpression(@"^(?=.*?[a-z])(?=.*?[0-9]).{6,}$", // This is a very weak password requirement to make the demo easy to review. You should enforce stronger requirements
+            ErrorMessage = "Password should have •  At least one lower case letter • At least one digit • Minimum 6 in length")]
+        */
         public string Password { get; set; }
         [Required]
         public string Gender { get; set; }
